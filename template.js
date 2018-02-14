5e0d3bb3c6124348a530cea64daab5355eac7b0b
Richard Boon <ric.ric.boon@gmail.com>
2018-02-14 12:17:43 +0100

    no message

--- template.js
+++ template.js
@@ -1,5 +1,14 @@
 let templates = {
     questions: function (title = "", statement = "") {
         return "<section><header><h2>" + title + "</h2>Stelling: " + statement + "</header><button type='button' id='pro'>Eens</button><button type='button' id='ambivalent'>Geen van beiden</button><button type='button' id='contra'>Oneens</button><button type='button' id='noOpinion'>Geen mening</button>"
+    },
+    result: function (result, totalSubjects) {
+        let resultHtml;
+
+        result.forEach((party) => {
+            resultHtml += "<div>" + party.name + " (" + Math.ceil(party.count/totalSubjects*100) + "%)</div>"
+        });
+
+        return "<section><header><h2>Resultaat</h2></header>" + resultHtml + "</section>"
     }
 };
